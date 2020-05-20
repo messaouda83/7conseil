a9c088fe490b06bad26bc9d62bdb409b649748de
messaouda BENCHIKH <57708302+messaouda83@users.noreply.github.com>
2020-05-20 09:23:25 +0200

    integration du css et js au theme wordpress

--- wp-content/themes/7advice/functions.php
+++ wp-content/themes/7advice/functions.php
@@ -12,6 +12,8 @@ add_action('wp_enqueue_scripts', 'load_css');
 /*charger Javascript*/
 
 function load_js(){
-
+wp_enqueue_script('jquery');
 wp_register_script('bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', 'jquery', false, true );
+wp_enqueue_script('bootstrap');
 }
+add_action('wp_enqueue_scripts', 'load_js');
