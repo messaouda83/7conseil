96890c7c1b38b24fb35f845ac5ad7e7c575c9247
messaouda BENCHIKH <57708302+messaouda83@users.noreply.github.com>
2020-05-22 10:24:42 +0200

    Ajout code pour top menu et menu mobile au fichier functions.php

--- wp-content/themes/7advice/functions.php
+++ wp-content/themes/7advice/functions.php
@@ -1,19 +1,37 @@
 <?php 
-/* charger le style css de bootstrap*/
+// Load stylesheet
 
 function load_css(){
 
 
     wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css' , array() , false, 'all');
     wp_enqueue_style('bootstrap');
+    wp_register_style('main', get_template_directory_uri() . '/css/main.css' , array() , false, 'all');
+    wp_enqueue_style('main');
 }
-add_action('wp_enqueue_scripts', 'load_css');
+    add_action('wp_enqueue_scripts', 'load_css');
 
-/*charger Javascript*/
+//Load Javascript
 
 function load_js(){
+
 wp_enqueue_script('jquery');
 wp_register_script('bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', 'jquery', false, true );
 wp_enqueue_script('bootstrap');
 }
+
 add_action('wp_enqueue_scripts', 'load_js');
+// Theme options 
+
+add_theme_support('menus');
+
+// Menu
+register_nav_menus(
+
+array(
+    'top-menu'=>'Top menu location',
+    'mobile-menu'=>'mobile menu location',
+)
+
+
+);
