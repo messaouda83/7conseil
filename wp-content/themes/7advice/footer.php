1447ad30e0eafbfaca28edcba9e9b6976ca5de0f
messaouda BENCHIKH <57708302+messaouda83@users.noreply.github.com>
2020-05-25 20:23:41 +0200

    modification dans des fichiers wordpress

--- wp-content/themes/7advice/footer.php
+++ wp-content/themes/7advice/footer.php
@@ -1,4 +1,15 @@
 <?php wp_footer(); ?>
+<div class="container">
+<?php
+  wp_nav_menu(
 
+    array(
+        'theme_location'=>'footer-menu',
+        //'menu'=>'top bar',
+        'menu_class'=>'footer-bar',
+    )
+  );
+?>
+</div>
 </body>
 </html>
