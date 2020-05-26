895b4bbf3c6bd935e76f7560e227edd6076b85f8
messaouda BENCHIKH <57708302+messaouda83@users.noreply.github.com>
2020-05-26 11:25:57 +0200

    Supprimer balise <h1> dans fichier archive.php

--- wp-content/themes/7advice/archive.php
+++ wp-content/themes/7advice/archive.php
@@ -3,7 +3,7 @@
 <section class="page-wrap">
 
     <div class="container">
-    <h1><?php the_title(); ?></h1>
+    
     <?php get_template_part('includes/section', 'archive'); ?>
     </div>
     
