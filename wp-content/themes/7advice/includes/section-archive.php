a5be4956bf39d89dc86ba19070e2ca64ef9a95ee
messaouda BENCHIKH <57708302+messaouda83@users.noreply.github.com>
2020-05-26 11:28:12 +0200

    Ajout des permalink et the_excerpt à la place the_content dans fichier section-archive.php

--- wp-content/themes/7advice/includes/section-archive.php
+++ wp-content/themes/7advice/includes/section-archive.php
@@ -1,5 +1,7 @@
 
 <?php
 if( have_posts() ): while( have_posts() ): the_post(); ?>
-    <?php the_content(); ?>
+<h3><?php the_title(); ?></h3>
+    <?php the_excerpt(); ?>
+    <a href="<?php the_permalink(); ?>">Lire plus...</a>
 <?php endwhile; else: endif;?>
