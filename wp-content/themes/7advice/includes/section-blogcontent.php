a906706a157d1d2111685aba9a02682fb3d5fd76
messaouda BENCHIKH <57708302+messaouda83@users.noreply.github.com>
2020-05-28 13:45:31 +0200

    ajouter la date et author name au blog

---
+++ wp-content/themes/7advice/includes/section-blogcontent.php
@@ -0,0 +1,27 @@
+
+<?php
+if( have_posts() ): while( have_posts() ): the_post(); ?>
+   <p> <?php echo get_the_date('d/m/Y h:i:s');?> </p>
+    <?php the_content(); ?>
+    <?php 
+    $fname = get_the_author_meta('first_name');
+    $lname = get_the_author_meta('last_name');
+    ?>
+  <p>Posted by  <?php echo $fname; ?> <?php echo $lname; ?></p>
+  <?php 
+  $tags = get_the_tags();
+  foreach($tags as $tag): ?>
+  <a href=" <?php echo get_tag_link($tag->term_id);?>" class="badge badge-success">
+<?php echo $tag->name; ?>
+  </a>
+  <?php endforeach; ?>
+
+  <?php 
+  $categories = get_the_category();
+  foreach($categories as $cat):?>
+<a href="<?php echo get_category_link($cat->term_id);?>">
+<?php echo $cat->name; ?>
+
+</a>
+  <?php endforeach; ?>
+<?php endwhile; else: endif;?>
