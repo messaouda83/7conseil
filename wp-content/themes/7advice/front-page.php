8f156088ce4cfe9759a3e6fee5479d638564cedf
messaouda BENCHIKH <57708302+messaouda83@users.noreply.github.com>
2020-05-28 13:32:06 +0200

    Ajouter marge top au fichier front-page.php

--- wp-content/themes/7advice/front-page.php
+++ wp-content/themes/7advice/front-page.php
@@ -1,5 +1,5 @@
 <?php get_header();?>
-<section class="page-wrap">
+<section class="page-wrap mt-5">
 <div class="container">
 <h1><?php the_title(); ?></h1>
 <?php get_template_part('includes/section', 'content'); ?>
