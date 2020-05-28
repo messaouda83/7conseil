e709a0129874653772d98954166f779a55a06d4e
messaouda BENCHIKH <57708302+messaouda83@users.noreply.github.com>
2020-05-28 13:39:35 +0200

    Supprimer l'affichage de menu bas de page

--- wp-content/themes/7advice/footer.php
+++ wp-content/themes/7advice/footer.php
@@ -1,15 +1,15 @@
 <?php wp_footer(); ?>
 <div class="container">
 <?php
-  wp_nav_menu(
+ /*  wp_nav_menu(
 
     array(
-        'theme_location'=>'footer-menu',
-        //'menu'=>'top bar',
-        'menu_class'=>'footer-bar',
+         'theme_location'=>'footer-menu', 
+        'menu'=>'top bar',
+        'menu_class'=>'footer-bar', 
     )
-  );
-?>
+  ); */
+?> 
 </div>
 </body>
 </html>
