63a6229268a75ce979604662766df3374877f28b
messaouda BENCHIKH <57708302+messaouda83@users.noreply.github.com>
2020-05-30 10:25:08 +0200

    Ajout post thumbnails au fichier single.php

--- wp-content/themes/7advice/single.php
+++ wp-content/themes/7advice/single.php
@@ -3,8 +3,12 @@
 <section class="page-wrap mt-5">
 
     <div class="container">
+        <?php if(has_post_thumbnail()): ?>
+            <img src="<?php the_post_thumbnail_url('blog-small');?>" alt ="<?php the_title(); ?>"class="img-fluid mb-3 img-thumbnail">
+        <?php endif;?>
     <h1><?php the_title(); ?></h1>
     <?php get_template_part('includes/section', 'blogcontent'); ?>
+    <?php wp_link_pages(); ?>
     </div>
     
 </section>
