c1558a83ba988d1b3dabf1e65e35153c9f6cadf2
messaouda BENCHIKH <57708302+messaouda83@users.noreply.github.com>
2020-05-30 14:42:48 +0200

    Ajouter page sidebar au fichier page.php

--- wp-content/themes/7advice/page.php
+++ wp-content/themes/7advice/page.php
@@ -1,8 +1,18 @@
 <?php get_header();?>
 <section class="page-wrap mt-5">
 <div class="container">
-<h1><?php the_title(); ?></h1>
-<?php get_template_part('includes/section', 'content'); ?>
+<section class="row">
+<div class="col-lg-3">
+        <?php if(is_active_sidebar('page-sidebar')):?>
+        <?php dynamic_sidebar('page-sidebar'); ?>
+        <?php endif;?>
+</div>
+<div class="col-lg-9">
+        <h1><?php the_title(); ?></h1>
+        <?php get_template_part('includes/section', 'content'); ?>
+</div>
+
+</section>
 </div>
 </section>
 <?php get_footer();?>
