e2fdd4802376ee062acffee5ba788bee5d831910
messaouda BENCHIKH <57708302+messaouda83@users.noreply.github.com>
2020-06-02 12:55:52 +0200

    Création fichier section-searchresults.php pour afficher resultat de recherche

---
+++ wp-content/themes/7advice/includes/section-searchresults.php
@@ -0,0 +1,31 @@
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?><!--La boucle WordPress-->
+
+		<div class="card mb-2"><!--Ajoute une bordure et un margin bottom de 3-->
+
+			<div class="card-body d-flex justify-content-center align-items-center">
+
+				<?php if (has_post_thumbnail()) : ?><!--Si possède une image-->
+
+					<img src="<?php the_post_thumbnail_url('blog-small'); ?>" alt="<?php the_title(); ?>" class="img-fluid mb-3 img-thumbnail  mr-4"><!--Affiche l'image-->
+
+				<?php endif; ?>
+
+				<div class="blog-content">
+
+					<h3><?php the_title(); ?></h3>
+
+					<?php the_excerpt(); ?>
+
+					<a href="<?php the_permalink(); ?>" class="btn btn-success">Lire plus</a><!--Ajoute un lien-->
+
+				</div>
+
+			</div>
+
+		</div>
+
+<?php endwhile; else: ?> 
+
+	Il n'y a pas de résultats '<?php echo get_search_query();?>'
+
+<?php endif; ?> 
