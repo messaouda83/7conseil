b2191cc4b53f3b68a630dcc8c8bfbaae1ebd69c0
messaouda BENCHIKH <57708302+messaouda83@users.noreply.github.com>
2020-06-08 15:23:48 +0200

    Ajout des fonctionalités pour le slideshow

--- wp-content/plugins/slideshow/slideshow.php
+++ wp-content/plugins/slideshow/slideshow.php
@@ -6,7 +6,12 @@ Version: 0.1
 Author: Messaouda Benchikh
 */
 
+use function PHPSTORM_META\type;
+
 add_action('init','slideshow_init' );
+add_action('add_meta_boxes', 'slideshow_metaboxes');
+add_action('save_post', 'slideshow_savepost', 10, 2);
+
 function slideshow_init(){
     $labels= array(
 
@@ -37,13 +42,46 @@ function slideshow_init(){
     /* Ajouter la taille de l'image */
     add_image_size('slider', 1200, 600, true);
 }
+
+/* Permet de gérer les metaboxes */
+function slideshow_metaboxes(){
+
+    add_meta_box('slideshow', 'Lien', 'slideshow_metabox', 'slide', 'normal', 'high');
+}
+/* Meta box pour gérer lesliens */
+function slideshow_metabox($object){
+    wp_nonce_field('slideshow', 'slideshow_nonce');
+    ?>
+<div class="meta-box-item-title">
+<h4>Lien de ce slide</h4>
+</div>
+<div class="meta-box-item-content">
+<input type="text" name="slideshow_link" style="width:100%;" value="<?= get_post_meta($object->ID, '_link', true); ?>">
+</div>
+    <?php
+
+
+}
+function slideshow_savepost($post_id, $post){
+if(!isset($_POST['slideshow_link']) || !wp_verify_nonce($_POST['slideshow_nonce'], 'slideshow')){
+return $post_id;
+}
+$type= get_post_type_object($post->post_type);
+if(current_user_can($type->cap->edit_post)){
+    return $post_id; /* ne permet pas d'editer les post meta par quelqu'un */
+
+
+}
+update_post_meta($post_id,'_link', $_POST['slideshow_link']);
+
+}
    /* Permet d'afficher le slide */
 function slideshow_show($limit=10){
     /* on importe javascript */
     wp_deregister_script('jquery');
     wp_enqueue_script('jquery','https://code.jquery.com/jquery-3.5.1.min.js', array('jquery'), '3.5.1', true);
     wp_enqueue_script('caroufredsel',plugins_url().'/slideshow/js/jquery.carouFredSel-6.2.1-packed.js', null, '6.2.1', true);
-    add_action('wp_footer','slideshow_script');
+    add_action('wp_footer','slideshow_script', 30);
 
     /*on écrit le code html */
 $slides =new WP_Query("post_type= slide&posts_per_page=$limit");
@@ -53,7 +91,8 @@ while($slides->have_posts()){
     $slides->the_post();
     /*Récupération les informations contenant l'article */
     global $post;
-    the_post_thumbnail('slider');
+
+    the_post_thumbnail('slider',array('style' =>'width:2000px!important;'));
     
 }
 echo '</div>';
@@ -61,6 +100,9 @@ echo '</div>';
 function slideshow_script(){
     ?>
     <script type="text/javascript">
+    (function($){
+        $('#slideshow').caroufredsel();
+    } )(jQuery);
     </script>
     <?php
 }
