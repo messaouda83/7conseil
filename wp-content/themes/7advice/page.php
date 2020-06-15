7a28f6edcd75d13f931e7dcf42b4144f5d8de680
messaouda BENCHIKH <57708302+messaouda83@users.noreply.github.com>
2020-06-15 14:42:48 +0200

    Ajouter margin buttom au titre de page dans page.php

--- wp-content/themes/7advice/page.php
+++ wp-content/themes/7advice/page.php
@@ -2,13 +2,13 @@
 <section class="page-wrap mt-5 ">
 <div class="container">
 <section class="row">
-<div class="col-lg-3">
-        <?php if(is_active_sidebar('page-sidebar')):?>
-        <?php dynamic_sidebar('page-sidebar'); ?>
-        <?php endif;?>
-</div>
+<!-- <div class="col-lg-3">
+        if(is_active_sidebar('page-sidebar')):
+         dynamic_sidebar('page-sidebar'); 
+         endif;
+</div> -->
 <div class="col-lg-9">
-        <h1><?php the_title(); ?></h1>
+        <h1 class="mb-3"><?php the_title(); ?></h1>
         <?php get_template_part('includes/section', 'content'); ?>
 </div>
 
