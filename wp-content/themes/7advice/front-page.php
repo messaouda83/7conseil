474206a4c89b237367d4853cd8914872fa63bfd0
messaouda BENCHIKH <57708302+messaouda83@users.noreply.github.com>
2020-06-15 16:25:34 +0200

    Ajout balise canvas dans front-page.php

--- wp-content/themes/7advice/front-page.php
+++ wp-content/themes/7advice/front-page.php
@@ -1,9 +1,30 @@
 <?php get_header();?>
 <!--code php slideshow_show(); -->
+<div class="context">
+        <h1>Bienvenue au Seven Conseil</h1>
+       
+</div>
+
+
+<div class="area" >
+<canvas></canvas>
+            <ul class="circles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+            </ul>
+</div >
 <section class="page-wrap ">
 <!-- echo do_shortcode('[smartslider3 slider="1"]'); code php-->
 <div class="container">
-<h1><?php the_title(); ?></h1>
+<h1 class="mb-3"><?php the_title(); ?></h1>
 <?php get_template_part('includes/section', 'content'); ?>
 </div>
 </section>
