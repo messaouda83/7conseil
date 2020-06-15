6194ba1e44e5a8de90f5b85764e4288ab2d49cd0
messaouda BENCHIKH <57708302+messaouda83@users.noreply.github.com>
2020-06-15 16:36:36 +0200

    Ajout margin-buttom pour titre de la page

--- wp-content/themes/7advice/single.php
+++ wp-content/themes/7advice/single.php
@@ -6,9 +6,9 @@
         <?php if(has_post_thumbnail()): ?>
             <img src="<?php the_post_thumbnail_url('blog-small');?>" alt ="<?php the_title(); ?>"class="img-fluid mb-3 img-thumbnail">
         <?php endif;?>
-    <h1><?php the_title(); ?></h1>
-    <?php get_template_part('includes/section', 'blogcontent'); ?>
-    <?php wp_link_pages(); ?>
+    <h1 class="mb-3"><?php the_title(); ?></h1>
+        <?php get_template_part('includes/section', 'blogcontent'); ?>
+        <?php wp_link_pages(); ?>
     </div>
     
 </section>
