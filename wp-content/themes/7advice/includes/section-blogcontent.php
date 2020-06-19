526bc5225504f894c800742b541939155afe1d15
messaouda BENCHIKH <57708302+messaouda83@users.noreply.github.com>
2020-06-19 11:22:51 +0200

    Ajout class text-info pour date et nom de l'auteur

--- wp-content/themes/7advice/includes/section-blogcontent.php
+++ wp-content/themes/7advice/includes/section-blogcontent.php
@@ -1,13 +1,13 @@
 
 <?php
 if( have_posts() ): while( have_posts() ): the_post(); ?>
-   <p> <?php echo get_the_date('d/m/Y h:i:s');?> </p>
+   <p class="text-info"> <?php echo get_the_date('d/m/Y h:i:s');?> </p>
     <?php the_content(); ?>
     <?php 
     $fname = get_the_author_meta('first_name');
     $lname = get_the_author_meta('last_name');
     ?>
-  <p>Posted by  <?php echo $fname; ?> <?php echo $lname; ?></p>
+  <p class="text-info">Posted by  <?php echo $fname; ?> <?php echo $lname; ?></p>
   <?php 
   $tags = get_the_tags();
   foreach($tags as $tag): ?>
