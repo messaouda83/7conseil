887fb7f9019531529a613b1f6c253532d27589f3
messaouda BENCHIKH <57708302+messaouda83@users.noreply.github.com>
2020-06-19 11:26:35 +0200

    thumbnail_url blog large

--- wp-content/themes/7advice/single.php
+++ wp-content/themes/7advice/single.php
@@ -4,7 +4,7 @@
 
     <div class="container">
         <?php if(has_post_thumbnail()): ?>
-            <img src="<?php the_post_thumbnail_url('blog-small');?>" alt ="<?php the_title(); ?>"class="img-fluid mb-3 img-thumbnail">
+            <img src="<?php the_post_thumbnail_url('blog-');?>" alt ="<?php the_title(); ?>"class="img-fluid mb-3 img-thumbnail">
         <?php endif;?>
     <h1 class="mb-3"><?php the_title(); ?></h1>
         <?php get_template_part('includes/section', 'blogcontent'); ?>
