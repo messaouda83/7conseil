7a117ed940d6840c9d367df56997a3da98d140c8
messaouda BENCHIKH <57708302+messaouda83@users.noreply.github.com>
2020-06-19 10:35:15 +0200

    Centrer  le titre de categorie de blog

--- wp-content/themes/7advice/category-blog.php
+++ wp-content/themes/7advice/category-blog.php
@@ -3,7 +3,7 @@
 <section class="page-wrap mt-5">
 
     <div class="container">
-    <h1> <?php echo single_cat_title();?>  </h1>
+    <h1 class="text-center"> <?php echo single_cat_title();?>  </h1>
     <?php get_template_part('includes/section', 'archive'); ?>
    <?php previous_posts_link(); ?>
    <?php next_posts_link(); ?>
