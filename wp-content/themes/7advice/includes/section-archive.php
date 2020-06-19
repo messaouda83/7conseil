eac52b126a97a1f7b4fd752076d209c5b70783f8
messaouda BENCHIKH <57708302+messaouda83@users.noreply.github.com>
2020-06-19 10:42:41 +0200

    Ajout class btn-primary

--- wp-content/themes/7advice/includes/section-archive.php
+++ wp-content/themes/7advice/includes/section-archive.php
@@ -3,9 +3,9 @@
 if( have_posts() ): while( have_posts() ): the_post(); ?>
 <div class="card mb-3">
     <div class="card-body">
-<h3><?php the_title(); ?></h3>
-    <?php the_excerpt(); ?>
-    <a href="<?php the_permalink(); ?>" class="btn btn-primary text-color">Lire plus...</a>
+        <h3><?php the_title(); ?></h3>
+        <?php the_excerpt(); ?>
+        <a href="<?php the_permalink(); ?>" class="btn btn-primary text-color">Lire plus...</a>
     </div>
  </div>
 <?php endwhile; else: endif;?>
